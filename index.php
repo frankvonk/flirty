6cf8584d8b01f10d43ad5414e04dabe116271a5e
Frank Vonk <frank.vonk@signify.com>
2024-11-17 21:26:31 +0100

    update positioning bg images

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-   <!--
+  <!--
   <meta http-equiv="refresh" content="2">
   -->
-  <meta charset=utf-8>
+  <meta charset="UTF-8">
   <title>Sun Gym</title>
 
   <link rel="shortcut icon" type="image/x-icon" href="./assets/icons/favicon7.ico" />
@@ -18,18 +18,15 @@
   <link href='stylesheet_the-white-lotus.css?ver=34' rel='stylesheet'>
   <link href='stylesheet_the-neon-demon.css?ver=34' rel='stylesheet'>
   <link href='stylesheet_irina-rimes.css?ver=35' rel='stylesheet'>
-  
-  <meta name="viewport" content="width=device-width">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
   <script src="./config.js"></script>
-  <style>
-@import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap');
-</style>
-
-
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap"
+    rel="stylesheet">
 
 </head>
 
@@ -40,8 +37,8 @@
 <body id='body'>
   <div class="lockOverlay" id="lockOverlay"></div>
 
-<!-- This is where title will be displayed -->
-<div id=listtitles></div>
+  <!-- This is where title will be displayed -->
+  <div id=listtitles></div>
 
 
 
@@ -53,12 +50,10 @@
 
 
   <div id="saving-spinner_parent" class="saving-spinner_parent">
-    <p id="saving-spinner" class="saving-spinner" >Saving</p>
+    <p id="saving-spinner" class="saving-spinner">Saving</p>
   </div>
 
-  <div id="retrieving-data-spinner"
-    class="retrieving-data-spinner"
-  >
+  <div id="retrieving-data-spinner" class="retrieving-data-spinner">
     <p>
       Getting your precious data for you!
     </p>
@@ -71,125 +66,94 @@
 
 
 
-  
-
-
-    <div id="action-bar" class="action-bar">
-      <!-- <h3 id="displayBioRobot" class="biorobot-title">
+  <div id="action-bar" class="action-bar">
+    <!-- <h3 id="displayBioRobot" class="biorobot-title">
         Биоробот делать / BioRobot ToDo 
       </h3> -->
-      <button class='action-bar_button' id="importDB" onclick=importDataBase()>
-        In
-      </button>
-      <button class='action-bar_button' id="exportDB" onclick=exportDataBase()>
-        Out
-      </button>
-      <button class='action-bar_button' id="bin" onclick=renderTasksInBin()>
-        Bin
-      </button>
-      <button class='action-bar_button' id="history" onclick=renderTasksInHistory()>
-        Done
-      </button>
-      <button class='action-bar_button' onclick=toggleDisplayOfAllCategoryTasks()>
-        Toggle
-      </button> 
-      <div class='action-bar_item counterTasks' id="counterTasks">
-      </div>
-      <a href="https://www.frankvonk.be">
-        <canvas id="signature" class="signature"></canvas>
-      </a>
-      <!-- <h4 class="hcBlack">Hide</h4> -->
+    <button class='action-bar_button' id="importDB" onclick=importDataBase()>
+      In
+    </button>
+    <button class='action-bar_button' id="exportDB" onclick=exportDataBase()>
+      Out
+    </button>
+    <button class='action-bar_button' id="bin" onclick=renderTasksInBin()>
+      Bin
+    </button>
+    <button class='action-bar_button' id="history" onclick=renderTasksInHistory()>
+      Done
+    </button>
+    <button class='action-bar_button' onclick=toggleDisplayOfAllCategoryTasks()>
+      Toggle
+    </button>
+    <div class='action-bar_item counterTasks' id="counterTasks">
     </div>
+    <a href="https://www.frankvonk.be">
+      <canvas id="signature" class="signature"></canvas>
+    </a>
+    <!-- <h4 class="hcBlack">Hide</h4> -->
+  </div>
 
 
 
 
 
-    <!-- Start Modal for general usage -->
-    <table class="modal" id="modal">
-    </table>
-    <!-- End Modal for general usage -->
+  <!-- Start Modal for general usage -->
+  <table class="modal" id="modal">
+  </table>
+  <!-- End Modal for general usage -->
 
-   <!-- Start Toast for general usage -->
-   <table class="toast" id="toast">
-    </table>
-    <!-- End Toast for general usage -->
-
-
+  <!-- Start Toast for general usage -->
+  <table class="toast" id="toast">
+  </table>
+  <!-- End Toast for general usage -->
 
 
 
+  <div id="categoryContainer" class="categoryContainer">
 
-
-
-
-
-
-
-
-
-
-    <div id="categoryContainer" class="categoryContainer">
-
-      <div class="flow createNewTaskGrid" id="createNewTaskGrid">
-        <!-- 
+    <div class="flow createNewTaskGrid" id="createNewTaskGrid">
+      <!-- 
           <h3 class="create-new-task">Create new task</h3>
         -->
 
-   
 
-        <div id="newTaskCategoryGrid"
-          class="newTaskCategoryGrid">
-        </div>
-         
-      
-        <div class="newTaskTitleInputParent">
-          <div class="newTaskTitleInputLabel">Title:</div>
-          <textarea class="newTaskTitleTextArea" 
-            type='text'
-            id="newTaskTitleTextArea"
-            placeholder="Write title here"></textarea>
-          <button class="new-task_submit" id="submit">
-            Submit
-          </button>
-        </div>
-
-      
-      
-      
-    
-        <div class="default-order-new-task_parent">
-          <button id="btnSetTaskFirst" class='default-order-new-task-first default-order-new-task'>
-            Set First
-          </button>
-          <button id="btnSetTaskLast" class='default-order-new-task-last default-order-new-task'>
-            Set Last
-          </button>
-        </div>
-
-        <div
-          class="new-cat_parent"
-        >
-          <div 
-            class="new-cat_label"
-          >
-            Create new Category
-          </div>
-          <input class="new-cat_input"
-              type='text'
-              id="newCategoryInput"
-              maxlength="40"
-              placeholder="...">
-          <button class="new-cat_submit" 
-            onclick="createNewCategory()"
-            id="createNewCategory"
-          >
-            Submit
-          </button>  
-        </div>
-
+      <div id="newTaskCategoryGrid" class="newTaskCategoryGrid">
       </div>
+
+
+      <div class="newTaskTitleInputParent">
+        <div class="newTaskTitleInputLabel">Title:</div>
+        <textarea class="newTaskTitleTextArea" type='text' id="newTaskTitleTextArea"
+          placeholder="Write title here"></textarea>
+        <button class="new-task_submit" id="submit">
+          Submit
+        </button>
+      </div>
+
+
+
+
+      <div class="default-order-new-task_parent">
+        <button id="btnSetTaskFirst" class='default-order-new-task-first default-order-new-task'>
+          Set First
+        </button>
+        <button id="btnSetTaskLast" class='default-order-new-task-last default-order-new-task'>
+          Set Last
+        </button>
+      </div>
+
+      <div class="new-cat_parent">
+        <div class="new-cat_label">
+          Create new Category
+        </div>
+        <input class="new-cat_input" type='text' id="newCategoryInput" maxlength="40" placeholder="...">
+        <button class="new-cat_submit" onclick="createNewCategory()" id="createNewCategory">
+          Submit
+        </button>
+      </div>
+
     </div>
+  </div>
 
 
 
@@ -198,100 +162,98 @@
 
 
 
-    <footer class="">
-      <div class="top">
-        <div class="createdBy">Created and hosted by
-          <a target="blank" href="http://www.frankvonk.be/">
-            www.frankvonk.be
-          </a>
-        </div>
-        <button id="settings-button"
-          class="settings-button"
-          onclick="openSettingsMenu()"
-        >⚙️</button>
-        
+  <footer class="">
+    <div class="top">
+      <div class="createdBy">Created and hosted by
+        <a target="blank" href="http://www.frankvonk.be/">
+          www.frankvonk.be
+        </a>
       </div>
-      <div class="bottom">
-        <p>Select a Styling Theme</p>
-        <button class="css-theme-btn css-theme-btn__sun-gym" id="css-theme-btn__sun-gym"
-          onclick="changeCSSTheme('sun-gym')">
-          SUN GYM
-        </button>
-        <button class="css-theme-btn css-theme-btn__black" id="css-theme-btn__black"
-          onclick="changeCSSTheme('black')">
-          Black
-        </button>
-        <!-- <button class="css-theme-btn css-theme-btn__corbusier" id="css-theme-btn__corbusier"
+      <button id="settings-button" class="settings-button" onclick="openSettingsMenu()">
+        ⚙️</button>
+
+    </div>
+    <div class="bottom">
+      <p>Select a Styling Theme</p>
+      <button class="css-theme-btn css-theme-btn__sun-gym" id="css-theme-btn__sun-gym"
+        onclick="changeCSSTheme('sun-gym')">
+        SUN GYM
+      </button>
+      <button class="css-theme-btn css-theme-btn__black" id="css-theme-btn__black"
+        onclick="changeCSSTheme('black')">
+        Black
+      </button>
+      <!-- <button class="css-theme-btn css-theme-btn__corbusier" id="css-theme-btn__corbusier"
           onclick="changeCSSTheme('corbusier')">
           Corbusier
         </button> -->
-        <button class="css-theme-btn css-theme-btn__the-white-lotus" id="css-theme-btn__the-white-lotus"
-          onclick="changeCSSTheme('the-white-lotus')">
-          The White Lotus 🪷
-        </button>
-        <button class="css-theme-btn css-theme-btn__the-neon-demon" id="css-theme-btn__the-neon-demon"
-          onclick="changeCSSTheme('the-neon-demon')">
-          The Neon Demon
-        </button>
-        <button class="css-theme-btn css-theme-btn__irina-rimes" id="css-theme-btn__irina-rimes"
-          onclick="changeCSSTheme('irina-rimes')">
-          Irina Rimes
-        </button>
-        <!-- <button class="css-theme-btn css-theme-btn__green-spacious" id="css-theme-btn__green-spacious"
+      <button class="css-theme-btn css-theme-btn__the-white-lotus" id="css-theme-btn__the-white-lotus"
+        onclick="changeCSSTheme('the-white-lotus')">
+        The White Lotus 🪷
+      </button>
+      <button class="css-theme-btn css-theme-btn__the-neon-demon" id="css-theme-btn__the-neon-demon"
+        onclick="changeCSSTheme('the-neon-demon')">
+        The Neon Demon
+      </button>
+      <button class="css-theme-btn css-theme-btn__irina-rimes" id="css-theme-btn__irina-rimes"
+        onclick="changeCSSTheme('irina-rimes')">
+        Irina Rimes
+      </button>
+      <!-- <button class="css-theme-btn css-theme-btn__green-spacious" id="css-theme-btn__green-spacious"
           onclick="changeCSSTheme('green-spacious')">
           Green Spacious
         </button>        -->
-        <!-- <button class="css-theme-btn css-theme-btn__spirit-animal" id="css-theme-btn__spirit-animal"
+      <!-- <button class="css-theme-btn css-theme-btn__spirit-animal" id="css-theme-btn__spirit-animal"
           onclick="changeCSSTheme('spirit-animal')">
           Spirit Animal
         </button> -->
-     
-        <!-- <button class="css-theme-btn css-theme-btn__the-visual-dome" id="css-theme-btn__the-visual-dome"
+
+      <!-- <button class="css-theme-btn css-theme-btn__the-visual-dome" id="css-theme-btn__the-visual-dome"
           onclick="changeCSSTheme('the-visual-dome')">
           The Visual Dome
         </button> -->
-        <!-- <button class="css-theme-btn css-theme-btn__oppenheimer" id="css-theme-btn__oppenheimer"
+      <!-- <button class="css-theme-btn css-theme-btn__oppenheimer" id="css-theme-btn__oppenheimer"
           onclick="changeCSSTheme('oppenheimer')">
           Oppenheimer
         </button> -->
-        <!-- <button class="css-theme-btn css-theme-btn__mac-os" id="css-theme-btn__mac-os"
+      <!-- <button class="css-theme-btn css-theme-btn__mac-os" id="css-theme-btn__mac-os"
           onclick="changeCSSTheme('mac-os')">
           Mac OS
         </button> -->
-        <!-- <button class="css-theme-btn css-theme-btn__lebowski" id="css-theme-btn__lebowski"
+      <!-- <button class="css-theme-btn css-theme-btn__lebowski" id="css-theme-btn__lebowski"
           onclick="changeCSSTheme('lebowski')">
           Lebowski
         </button> -->
-        <button class="css-theme-btn css-theme-btn__wireframe" id="css-theme-btn__wireframe"
-          onclick="changeCSSTheme('wireframe')">
-          Wireframe
-        </button>    
-        <!-- <button class="css-theme-btn css-theme-btn__bender" id="css-theme-btn__bender"
+      <button class="css-theme-btn css-theme-btn__wireframe" id="css-theme-btn__wireframe"
+        onclick="changeCSSTheme('wireframe')">
+        Wireframe
+      </button>
+      <!-- <button class="css-theme-btn css-theme-btn__bender" id="css-theme-btn__bender"
           onclick="changeCSSTheme('bender')">
           Bender
         </button> -->
-        <!-- <button class="css-theme-btn css-theme-btn__vaporwave" id="css-theme-btn__vaporwave"
+      <!-- <button class="css-theme-btn css-theme-btn__vaporwave" id="css-theme-btn__vaporwave"
           onclick="changeCSSTheme('vaporwave')">
           Vaporwave
         </button> -->
-        <!-- <button class="css-theme-btn css-theme-btn__synthax" id="css-theme-btn__synthax"
+      <!-- <button class="css-theme-btn css-theme-btn__synthax" id="css-theme-btn__synthax"
           onclick="changeCSSTheme('synthax')">
           Synthax
         </button>    -->
-        <!-- <button class="css-theme-btn css-theme-btn__pastel" id="css-theme-btn__paster"
+      <!-- <button class="css-theme-btn css-theme-btn__pastel" id="css-theme-btn__paster"
           onclick="changeCSSTheme('pastel')">
           Pastel
         </button> -->
-      </div>
-      <button id="cssPlay" style="color: white;" onclick="modalForCSSPlay()">
-          CSS Play
-        </button>    
-    </footer>
+    </div>
+    <button id="cssPlay" style="color: white;" onclick="modalForCSSPlay()">
+      CSS Play
+    </button>
+  </footer>
 
 
 
 
-    <?php include 'appBioRobotToDo.php'; ?>
+  <?php include 'appBioRobotToDo.php'; ?>
 
 
 
@@ -306,78 +268,79 @@
 <script src="./react-background-animation/vanta.clouds.min.js"></script>
 <script src="./react-background-animation/vanta.waves.min.js"></script> -->
 <script>
-// VANTA.DOTS({
-//   el: "html",
-//   mouseControls: true,
-//   touchControls: true,
-//   gyroControls: false,
-//   minHeight: 200.00,
-//   minWidth: 200.00,
-//   scale: 1.00,
-//   scaleMobile: 1.00
-// })
-// VANTA.HALO({
-//   el: "html",
-//   mouseControls: true,
-//   touchControls: true,
-//   gyroControls: false,
-//   minHeight: 2000.00,
-  
-//   minWidth: 200.00
-// })
+  // VANTA.DOTS({
+  //   el: "html",
+  //   mouseControls: true,
+  //   touchControls: true,
+  //   gyroControls: false,
+  //   minHeight: 200.00,
+  //   minWidth: 200.00,
+  //   scale: 1.00,
+  //   scaleMobile: 1.00
+  // })
+  // VANTA.HALO({
+  //   el: "html",
+  //   mouseControls: true,
+  //   touchControls: true,
+  //   gyroControls: false,
+  //   minHeight: 2000.00,
 
-// VANTA.FOG({
-//   el: "html",
-//   mouseControls: true,
-//   touchControls: true,
-//   gyroControls: false,
-//   minHeight: 200.00,
-//   minWidth: 200.00,
-//   highlightColor: 0xee94ff,
-//   midtoneColor: 0xff94c3,
-//   lowlightColor: 0x80b0ff,
-//   baseColor: 0xffffff,
-//   blurFactor: 0.24
-// })
-// VANTA.CLOUDS({
-//   el: "html",
-//   mouseControls: true,
-//   touchControls: true,
-//   gyroControls: false,
-//   minHeight: 200.00,
-//   minWidth: 200.00,
-//   skyColor: 0xa5aff0,
-//   cloudColor: 0xe0b9e6,
-//   cloudShadowColor: 0xff3ce5,
-//   sunColor: 0xfff9a0
-// })
+  //   minWidth: 200.00
+  // })
 
-// VANTA.WAVES({
-//   el: "html",
-//   mouseControls: true,
-//   touchControls: true,
-//   gyroControls: false,
-//   minHeight: 200.00,
-//   minWidth: 200.00,
-//   scale: 1.00,
-//   scaleMobile: 1.00,
-//   color: 0xf082a5,
-//   shininess: 10.00,
-//   waveHeight: 10.00,
-//   waveSpeed: 0.90,
-//   zoom: 0.65
-// })
+  // VANTA.FOG({
+  //   el: "html",
+  //   mouseControls: true,
+  //   touchControls: true,
+  //   gyroControls: false,
+  //   minHeight: 200.00,
+  //   minWidth: 200.00,
+  //   highlightColor: 0xee94ff,
+  //   midtoneColor: 0xff94c3,
+  //   lowlightColor: 0x80b0ff,
+  //   baseColor: 0xffffff,
+  //   blurFactor: 0.24
+  // })
+  // VANTA.CLOUDS({
+  //   el: "html",
+  //   mouseControls: true,
+  //   touchControls: true,
+  //   gyroControls: false,
+  //   minHeight: 200.00,
+  //   minWidth: 200.00,
+  //   skyColor: 0xa5aff0,
+  //   cloudColor: 0xe0b9e6,
+  //   cloudShadowColor: 0xff3ce5,
+  //   sunColor: 0xfff9a0
+  // })
+
+  // VANTA.WAVES({
+  //   el: "html",
+  //   mouseControls: true,
+  //   touchControls: true,
+  //   gyroControls: false,
+  //   minHeight: 200.00,
+  //   minWidth: 200.00,
+  //   scale: 1.00,
+  //   scaleMobile: 1.00,
+  //   color: 0xf082a5,
+  //   shininess: 10.00,
+  //   waveHeight: 10.00,
+  //   waveSpeed: 0.90,
+  //   zoom: 0.65
+  // })
 
 
 
-// let els = document.getElementsByTagName('canvas')
-// setTimeout(() => {
-// // console.log(els[els.length-1])
-// // els[els.length-1].style.border = '1px solid red'
-// // els[els.length-1].style.height = '300vh'
-// // els[els.length-1].style.width = '300vw'
-// }, 3000);
+  // let els = document.getElementsByTagName('canvas')
+  // setTimeout(() => {
+  // // console.log(els[els.length-1])
+  // // els[els.length-1].style.border = '1px solid red'
+  // // els[els.length-1].style.height = '300vh'
+  // // els[els.length-1].style.width = '300vw'
+  // }, 3000);
 
 
 </script>
+
 </html>
